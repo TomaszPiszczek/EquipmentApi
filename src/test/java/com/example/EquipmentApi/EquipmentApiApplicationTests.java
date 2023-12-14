9ca0305c634e8294d8c3111d800f5f1099f26aef
TomaszPiszczek <115466543+TomaszPiszczek@users.noreply.github.com>
2023-12-14 09:27:59 +0100

    Initial commit

---
+++ src/test/java/com/example/EquipmentApi/EquipmentApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EquipmentApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EquipmentApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
